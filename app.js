b228630c96d70acbc761fa705444d9560f21cb09
remony <stuartjdouglas@icloud.com>
2017-05-25 22:17:14 +0100

    removed unused library and moved to minified libraries

--- app.js
+++ app.js
@@ -17,17 +17,6 @@ window.onload = function() {
                     this.repos = response.body;
                     this.repos.sort((a, b) => b['updated_at'].localeCompare(a['updated_at']));
 
-
-                    this.repos.forEach(function(r) {
-                        var c = (r.name & 0x00FFFFFF)
-                            .toString(16)
-                            .toUpperCase();
-                        r.tabcolor = '1px solid #' + md5(r.name).slice(0, 6);
-                        r.color = '#' + md5(r.name).slice(0, 6);
-                        r.name.replace(/\-/g, ' ');
-                    })
-
-
                     this.repos = this.repos.slice(0, 8);
 
                 }, response => {
