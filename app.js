48d8bd2201d787f84970b5165f780ed9925d134c
remony <stuartjdouglas@icloud.com>
2017-05-25 22:20:56 +0100

    No longer waiting for window to load before performing vue actiosn

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
-console.log('heyu')
-window.onload = function() {
-    var app = new Vue({
-        el: '#app',
-        data: {
-            loading: 'Software developer at NCR; Anime addict; Photography hobbyist.'
+var app = new Vue({
+    el: '#app',
+    data: {
+        loading: 'Software developer at NCR; Anime addict; Photography hobbyist.'
+    }
+});
+var github = new Vue({
+    el: '#github',
+    methods: {
+        getData: function() {
+            this.$http.get('https://api.github.com/users/remony/repos').then(response => {
+
+                // get body data
+                console.log(response.body);
+                this.repos = response.body;
+                this.repos.sort((a, b) => b['updated_at'].localeCompare(a['updated_at']));
+
+                this.repos = this.repos.slice(0, 8);
+
+            }, response => {
+                // error callback
+            })
         }
-    });
-    var github = new Vue({
-        el: '#github',
-        methods: {
-            getData: function() {
-                this.$http.get('https://api.github.com/users/remony/repos').then(response => {
+    },
+    data: {
+        repos: 'Loading'
+    },
+    beforeMount() {
+        this.getData()
+    }
 
-                    // get body data
-                    console.log(response.body);
-                    this.repos = response.body;
-                    this.repos.sort((a, b) => b['updated_at'].localeCompare(a['updated_at']));
-
-                    this.repos = this.repos.slice(0, 8);
-
-                }, response => {
-                    // error callback
-                })
-            }
-        },
-        data: {
-            repos: 'Loading'
-        },
-        beforeMount() {
-            this.getData()
-        }
-
-    });
-
-};
+});
