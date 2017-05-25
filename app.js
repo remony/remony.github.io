995a34c61a42137e02590ef943138a4a1c87f793
remony <stuartjdouglas@icloud.com>
2017-05-25 22:10:32 +0100

    cleaned code, removed idea ide files

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
 console.log('heyu')
-window.onload = function () {
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Software developer at NCR; Anime addict; Photography hobbyist.'
-  }
-});
-var github = new Vue({
-  el: '#github',
-  methods: {
-    getData: function() {
-      this.$http.get('https://api.github.com/users/remony/repos').then(response => {
+window.onload = function() {
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Software developer at NCR; Anime addict; Photography hobbyist.'
+        }
+    });
+    var github = new Vue({
+        el: '#github',
+        methods: {
+            getData: function() {
+                this.$http.get('https://api.github.com/users/remony/repos').then(response => {
 
-        // get body data
-        console.log(response.body);
-        this.repos = response.body;
-        this.repos.sort((a, b) => b['updated_at'].localeCompare(a['updated_at']));
-
-
-        this.repos.forEach(function(r) {
-          var c = (r.name & 0x00FFFFFF)
-              .toString(16)
-              .toUpperCase();
-          r.tabcolor = '1px solid #' + md5(r.name).slice(0, 6);
-          r.color = '#' + md5(r.name).slice(0, 6);
-          r.name.replace(/\-/g,' ');
-        })
+                    // get body data
+                    console.log(response.body);
+                    this.repos = response.body;
+                    this.repos.sort((a, b) => b['updated_at'].localeCompare(a['updated_at']));
 
 
-        this.repos = this.repos.slice(0, 8);
+                    this.repos.forEach(function(r) {
+                        var c = (r.name & 0x00FFFFFF)
+                            .toString(16)
+                            .toUpperCase();
+                        r.tabcolor = '1px solid #' + md5(r.name).slice(0, 6);
+                        r.color = '#' + md5(r.name).slice(0, 6);
+                        r.name.replace(/\-/g, ' ');
+                    })
 
-      }, response => {
-        // error callback
-      })
-    }
-  },
-  data: {
-    repos: 'Loading'
-  },
- beforeMount(){
-    this.getData()
- }
 
-});
+                    this.repos = this.repos.slice(0, 8);
+
+                }, response => {
+                    // error callback
+                })
+            }
+        },
+        data: {
+            repos: 'Loading'
+        },
+        beforeMount() {
+            this.getData()
+        }
+
+    });
 
 };
