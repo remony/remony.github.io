7434c6174c150d4ec141e5b0a238d867bbf4137e
remony <stuartjdouglas@icloud.com>
2017-10-23 18:17:10 +0100

    init fun project for ali-con

---
+++ random/alicon/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        header: 'Genji-kun',
+        subHeader: 'A Celebration of Video Games, Anime and Cosplay',
+        date: '31/03/2018',
+        price: '£6 entry'
+    }
+})
