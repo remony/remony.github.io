709e734d12146a395cc0b76152f8fe884a8ff712
remony <stuartjdouglas@icloud.com>
2017-10-23 18:19:57 +0100

    improved performance

--- random/alicon/main.js
+++ random/alicon/main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
+        loaded: true,
         header: 'Genji-kun',
         subHeader: 'A Celebration of Video Games, Anime and Cosplay',
         date: '31/03/2018',
